f1ee9d4fde171fb7e0445ce905a8ed93c779ade8
edmaputra <bangun.edma.saputra@gmail.com>
2019-08-14 09:01:23 +0800

    add LogContextFilter class

---
+++ src/main/java/io/github/edmaputra/edtmplte/configuration/LogContextFilter.java
@@ -0,0 +1,36 @@
+package io.github.edmaputra.edtmplte.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+public class LogContextFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        String transactionId = UUID.randomUUID().toString();
+
+        MDC.put("TRANS_ID", transactionId);
+
+        try {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+        finally {
+            MDC.clear();
+        }
+    }
+}
