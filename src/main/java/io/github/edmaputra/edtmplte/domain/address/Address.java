a3f9f01cf13da01eda72419a5fb4a140d51f5ac5
bangun.saputra <bangun.saputra@xtremax.com>
2019-12-31 10:33:10 +0700

    update #1

--- src/main/java/io/github/edmaputra/edtmplte/domain/address/Address.java
+++ src/main/java/io/github/edmaputra/edtmplte/domain/address/Address.java
@@ -1,17 +1,41 @@
 package io.github.edmaputra.edtmplte.domain.address;
 
 import io.github.edmaputra.edtmplte.domain.ABaseIdEntity;
+import io.github.edmaputra.edtmplte.domain.person.Person;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.Set;
 
 public abstract class Address extends ABaseIdEntity {
 
+    @ManyToMany(cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE
+    }, mappedBy = "addresses")
+    private Set<Person> persons = new HashSet<>();
+
+    @NotBlank(message = "Street Name Cannot Null or Empty")
+    @Column(name = "street", length = 150, nullable = false)
     private String street;
 
+    @NotBlank(message = "City Name Cannot Null or Empty")
+    @Column(name = "city", length = 100, nullable = false)
     private String city;
 
+    @NotBlank(message = "Province Name Cannot Null or Empty")
+    @Column(name = "province", length = 100, nullable = false)
     private String province;
 
+    @NotBlank(message = "Zip Code Name Cannot Null or Empty")
+    @Column(name = "zip_code", length = 10, nullable = false)
     private String zipCode;
 
+    @NotBlank(message = "Country Name Cannot Null or Empty")
+    @Column(name = "country", length = 80, nullable = false)
     private String country;
 
     public String getStreet() {
@@ -54,10 +78,19 @@ public abstract class Address extends ABaseIdEntity {
         this.country = country;
     }
 
+    public Set<Person> getPersons() {
+        return persons;
+    }
+
+    public void addPerson(Person person) {
+        this.persons.add(person);
+    }
+
     @Override
     public String toString() {
         return "Address{" +
-                "street='" + street + '\'' +
+                "persons=" + persons +
+                ", street='" + street + '\'' +
                 ", city='" + city + '\'' +
                 ", province='" + province + '\'' +
                 ", zipCode='" + zipCode + '\'' +
--- src/main/java/io/github/edmaputra/edtmplte/domain/person/Person.java
+++ src/main/java/io/github/edmaputra/edtmplte/domain/person/Person.java
@@ -1,46 +1,80 @@
 package io.github.edmaputra.edtmplte.domain.person;
 
 import io.github.edmaputra.edtmplte.domain.ABaseIdEntity;
+import io.github.edmaputra.edtmplte.domain.address.Address;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 public abstract class Person extends ABaseIdEntity {
 
     private static final long serialVersionUID = 1L;
 
     @NotBlank(message = "First Name Cannot Null or Empty")
+    @Column(name = "first_name", length = 150)
     private String firstName = "";
 
     @NotNull(message = "Middle Name Cannot Null, Empty is acceptable")
+    @Column(name = "middle_name", length = 150, nullable = false)
     private String middleName = "";
 
     @NotNull(message = "Last Name Cannot Null, Empty is acceptable")
+    @Column(name = "last_name", length = 150, nullable = false)
     private String lastName = "";
 
     @NotNull(message = "Gender Cannot Null")
+    @Column(name = "gender", length = 25, nullable = false)
     private Gender gender = Gender.UNKNOWN;
 
     @NotNull(message = "Marital Status Cannot Null")
+    @Column(name = "marital_status", length = 25, nullable = false)
     private MaritalStatus maritalStatus = MaritalStatus.SINGLE;
 
     @NotNull(message = "Birth Place Cannot Null")
+    @Column(name = "birth_place", length = 70, nullable = false)
     private String birthPlace = "";
 
     @NotNull(message = "Birth Date Cannot Null")
+    @Column(name = "birth_date", nullable = false)
     private LocalDate birthDate = LocalDate.MIN;
+
+    @NotNull(message = "Phone Number Cannot Null")
+    @Column(name = "phone_number", length = 20, nullable = false)
+    private String phoneNumber;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE
+    })
+    @JoinTable(
+            name = "person_address",
+            joinColumns = { @JoinColumn(name = "person_id", nullable = false) },
+            inverseJoinColumns = { @JoinColumn(name = "address_id", nullable = false) }
+    )
+    private Set<Address> addresses = new HashSet<>();
 
     public Person() {
     }
 
-    public Person(@NotBlank(message = "First Name Cannot Null or Empty") String firstName,
-                  @NotNull(message = "Middle Name Cannot Null, Empty is acceptable") String middleName,
-                  @NotNull(message = "Last Name Cannot Null, Empty is acceptable") String lastName,
-                  @NotNull(message = "Gender Cannot Null") Gender gender,
-                  @NotNull(message = "Marital Status Cannot Null") MaritalStatus maritalStatus,
-                  @NotNull(message = "Birth Place Cannot Null") String birthPlace,
-                  @NotNull(message = "Birth Date Cannot Null") LocalDate birthDate) {
+    public Person(
+            @NotBlank(message = "First Name Cannot Null or Empty") String firstName,
+            @NotNull(message = "Middle Name Cannot Null, Empty is acceptable") String middleName,
+            @NotNull(message = "Last Name Cannot Null, Empty is acceptable") String lastName,
+            @NotNull(message = "Gender Cannot Null") Gender gender,
+            @NotNull(message = "Marital Status Cannot Null") MaritalStatus maritalStatus,
+            @NotNull(message = "Birth Place Cannot Null") String birthPlace,
+            @NotNull(message = "Birth Date Cannot Null") LocalDate birthDate,
+            @NotNull(message = "Phone Number Cannot Null") String phoneNumber
+    ) {
         this.firstName = firstName;
         this.middleName = middleName;
         this.lastName = lastName;
@@ -48,6 +82,7 @@ public abstract class Person extends ABaseIdEntity {
         this.maritalStatus = maritalStatus;
         this.birthPlace = birthPlace;
         this.birthDate = birthDate;
+        this.phoneNumber = phoneNumber;
     }
 
     public String getFirstName() {
@@ -106,6 +141,22 @@ public abstract class Person extends ABaseIdEntity {
         this.birthDate = birthDate;
     }
 
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Set<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void addAddress(Address address) {
+        this.addresses.add(address);
+    }
+
     @Override
     public String toString() {
         return "Person{" +
@@ -116,6 +167,7 @@ public abstract class Person extends ABaseIdEntity {
                 ", maritalStatus=" + maritalStatus +
                 ", birthPlace='" + birthPlace + '\'' +
                 ", birthDate=" + birthDate +
+                ", phoneNumber='" + phoneNumber + '\'' +
                 '}';
     }
 }
